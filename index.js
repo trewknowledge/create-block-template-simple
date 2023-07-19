9875695517aff87c329f6ad1c21e9f5def73bbab
Fernando Claussen <fclaussen@trewknowledge.com>
2023-07-19 17:13:16 -0400

    Adding template files and default values

---
+++ index.js
@@ -0,0 +1,13 @@
+const { join } = require( 'path' );
+
+module.exports = {
+  pluginTemplatesPath: join( __dirname, 'plugin-templates' ),
+  blockTemplatesPath: join( __dirname, 'block-templates' ),
+	defaultValues: {
+    pluginURI: 'https://trewknowledge.com',
+		version: '1.0.0',
+    author: 'Trew Knowledge',
+    namespace: 'tk',
+    category: 'design',
+  }
+};
